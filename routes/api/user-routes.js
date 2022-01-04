46257caf6616b2f636fec16b84c75f96888e4836
Edison Simondet <edison.simondet@gmail.com>
2022-01-03 22:34:53 -0600

    CRUD route for login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -34,7 +34,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
- 
+
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -45,6 +45,28 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/login', (req, res) => {
+  
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
 });
 
 router.put('/:id', (req, res) => {
